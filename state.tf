4a1b2dc3ff7e81ea0752c09f9cf5a45d3fd46d2f
Your Name <you@example.com>
2023-05-24 11:38:57 +0800

    Terraform

--- state.tf
+++ state.tf
@@ -1,25 +1,8 @@
-/*
-teraform {
-  backend "s3" {
-    bucket = "terraform-a72"
-    key = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
 
+
+/*terraform {
+  backend "s3" {
+
+  }
 }*/
-/*
-terraform {
-  backend "s3" {
-    bucket = "terraform-a72"
-    key    = "roboshop/dev/terraform.tfstate"
-    region = "us-east-1"
-  }
-
-}*/
-
-terraform {
-  backend "s3" {
-
-  }
-}
 
